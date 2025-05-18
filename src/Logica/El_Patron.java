db6661db86469e7d4f68d7a4848283249885fb60
Amet Perez <ametitoop52@gmail.com>
2025-05-18 17:41:07 -0600

    Add files via upload

---
+++ src/Logica/El_Patron.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+
+/**
+ *
+ * @author ameti
+ */
+public class El_Patron extends Empleado {
+
+    public El_Patron(String nombre, boolean Status, String area, double salario, String antiguedad, String ingreso) {
+        super(nombre, Status, area, salario, antiguedad, ingreso);
+    }
+}
